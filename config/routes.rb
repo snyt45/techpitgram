93447de019b963a630da64edb3220d752aa4562e
yutasano <snyt45@gmail.com>
2019-12-30 15:24:24 +0900

    Revert "Feature/devise setup"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { registrations: 'registrations' }
-
-  root 'pages#home'
-
-  get '/users/:id', to: 'users#show', as: 'user'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
